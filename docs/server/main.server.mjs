bc150661f94dd5c3a323616d1a130fa182c25528
subodh thapa <subodhthapa@subodhs-MBP.attlocal.net>
2025-09-16 20:29:15 -0500

    removed join the club button

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-VHCIF54N.mjs";import"./chunk-6LJOBULP.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-AQW4M3WO.mjs";import"./chunk-6LJOBULP.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
